be6d757480273caf7e3103357a55e8ed522a0fab
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-23 08:34:34 -0500

    added new photo and reorganized array of objects

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,14 +1,34 @@
 const galleryItems = [
     // { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
-    { id: 1, path: 'images/bike_path.jpg', description: 'Photo of Cedar Lake Trail, Bike Path.', likes: 0 }
-    { id: 2, path: 'images/Melissa.jpg', description: 'Picture of Melissa Ho', likes: 0 }
-    { id: 3, path: 'images/Nature.jpg', description: 'Picture of Katherine hugging a Tree', likes: 0 }
-    { id: 4, path: 'images/PublicArt.jpg', description: 'Picture of Iguana sculptures in Holland.', likes: 0 }
-    { id: 5, path: 'images/Travel.jpg', description: 'Picture of Me in Guangzhou China.', likes: 0 }
-    { id: 6, path: 'images/adventure.jpg', description: 'Picture of Katherine at Staunton State Park looking off into the distance.', likes: 0 }
+    { id: 1, 
+        path: 'images/bike_path.jpg', 
+        description: 'Photo of Cedar Lake Trail, Bike Path.', 
+        likes: 0 }
 
+    { id: 2, 
+        path: 'images/Melissa.jpg', 
+        description: 'Picture of Melissa Ho', 
+        likes: 0 }
 
+    { id: 3, 
+        path: 'images/Parents.jpg', 
+        description: 'Picture of my Mother and Father eating delicious food at The Weinery while visiting me in Minneapolis', 
+        likes: 0 }
 
+    { id: 4, 
+        path: 'images/PublicArt.jpg', 
+        description: 'Picture of Iguana sculptures in Holland.', 
+        likes: 0 }
+
+    { id: 5, 
+        path: 'images/Travel.jpg', 
+        description: 'Picture of Me in Guangzhou China.', 
+        likes: 0 }
+
+    { id: 6, 
+        path: 'images/adventure.jpg', 
+        description: 'Picture of Katherine at Staunton State Park looking off into the distance.', 
+        likes: 0 }
 ];
 
 module.exports = galleryItems;
