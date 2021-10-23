eb0066ec1de4cf4230f5247bccebe9bb6afcd448
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-23 15:33:50 -0500

    centered likespan under images

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,17 +1,23 @@
+import './GalleryItem.css'
+
 function GalleryItem({ image }) {
 
     return (
         <>
-            <div className="listDiv">
+            <view className="imgWithText" style={{ flex: 1, flexDirection: 'column' }}>
 
-                <img key={image.id} src={image.path} alt={image.description} width="175" height="200"></img>)
+                <div className="listDiv">
+                    <img onClick={image.onClick} key={image.id} src={image.path} alt={image.description} width="175" height="200"></img>
 
-                <span className="imageSpan">
-                    {/* {toDisplay()} */}
-                    <button onClick={() => onLike(image.id)}>LIKE</button>
-                    <p>{image.likes} likes</p>
-                </span>
-            </div>
+
+                    <span className="likeSpan">
+                        <button> LIKE </button>
+                        <p>{image.likes} likes </p>
+                        {/* onClick={() => onLike(image.id)} */}
+                    </span>
+                </div>
+            </view>
+
         </>
     )
 }
