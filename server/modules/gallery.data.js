55eb821b2c662b16ba90aefd976a058fc3a12196
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-23 09:24:40 -0500

    added GET to APP and tested

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -3,27 +3,27 @@ const galleryItems = [
     { id: 1, 
         path: 'images/bike_path.jpg', 
         description: 'Photo of Cedar Lake Trail, Bike Path.', 
-        likes: 0 }
+        likes: 0 },
 
     { id: 2, 
         path: 'images/Melissa.jpg', 
         description: 'Picture of Melissa Ho', 
-        likes: 0 }
+        likes: 0 },
 
     { id: 3, 
         path: 'images/Parents.jpg', 
         description: 'Picture of my Mother and Father eating delicious food at The Weinery while visiting me in Minneapolis', 
-        likes: 0 }
+        likes: 0 },
 
     { id: 4, 
         path: 'images/PublicArt.jpg', 
         description: 'Picture of Iguana sculptures in Holland.', 
-        likes: 0 }
+        likes: 0 },
 
     { id: 5, 
         path: 'images/Travel.jpg', 
         description: 'Picture of Me in Guangzhou China.', 
-        likes: 0 }
+        likes: 0 },
 
     { id: 6, 
         path: 'images/adventure.jpg', 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-    return (
+    
+  useEffect(() => {
+    fetchList ()
+  }, [])
+
+  let [galleryList, setGalleryList] = useState([]);
+  
+const fetchList = () => {
+  axios({ 
+    method: "GET", //using axios to GET data to App
+    url: '/gallery'
+  })
+  .then((response) => {
+    console.log(`entire response`, response);
+    setGalleryList(response.data)
+  })
+  .catch(function (error) {
+    console.log(`ERROR on GET `, error);
+  })
+} //end FETCH(GET)List 
+//tested GET in Postman Array or Objects returns
+
+
+  return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
